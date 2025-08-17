17a4f3b9ed31634133a7ce31846eddc4dc238fab
Amjad Abdullah <amerabd419@gmail.com>
2025-08-17 21:52:19 +0200

    Add notification system for validation and conversion feedback

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-function decimalToHex() {
-    const decimal = document.getElementById('decimal').value;
-    const hex = parseInt(decimal).toString(16).toUpperCase();
-    document.getElementById('hexResult').innerText = `Hex: ${hex}`;
+
+function decimalToHex(){
+    let decimal = document.getElementById('decimal').value;
+    if (decimal == ''){
+        showNotification('Bitte eine Dezimalzahl eingeben');
+        return;
+    }
+    let hex = parseInt(decimal).toString(16).toUpperCase();
+    document.getElementById('hexResult').textContent = 'Hex : ' + hex ;
 }
 
-function hexToDecimal() {
-    const hex = document.getElementById('hex').value;
-    const decimal = parseInt(hex, 16);
-    document.getElementById('decResult').innerText = `Decimal: ${decimal}`;
+function hexToDecimal(){
+    let hex = document.getElementById('hex').value;
+    if (hex == ''){
+        showNotification('Bitte eine Hexadezimalzahl eingeben');
+        return;
+    }
+    let decimal = parseInt(hex, 16);
+    if (isNaN(decimal)){
+        showNotification('Ungültige Zahl');
+        return;
+    }
+    document.getElementById('decResult').textContent = 'Dez : ' + decimal;
+}
+
+function goBack(){
+    window.history.go(-(window.history.length - 1));
+}
+function showNotification(message, color = '#e94444ff') {
+    let notif = document.getElementById('notification');
+    notif.textContent = message;
+    notif.style.background = color;
+    notif.classList.add('show');
+
+
+    setTimeout(() => {
+        notif.classList.remove('show');
+    }, 2000);
 }
